22eecb6cdf9fc35ebe34c99a0e73648fa9ac94a2
DatNguyenHoThe <nguyenhothedat88@gmail.com>
2025-05-16 14:59:55 +0700

    update to deploy

--- font-end/app/ui/MainBanners.tsx
+++ font-end/app/ui/MainBanners.tsx
@@ -9,43 +9,43 @@ export default function MainBanners() {
     const banners = [
         {
             name: 'thang_04_laptop_gaming_banner_web_slider_800x400',
-            imgUrl: 'http://localhost:8889/uploads/banners/thang_04_laptop_gaming_banner_web_slider_800x400.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/thang_04_laptop_gaming_banner_web_slider_800x400.webp',
         },
         {
             name: 'thang_04_laptop_acer',
-            imgUrl: 'http://localhost:8889/uploads/banners/thang_04_laptop_acer.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/thang_04_laptop_acer.webp',
         },
         {
             name: 'thang_04_banner_web_slider_800x400',
-            imgUrl: 'http://localhost:8889/uploads/banners/thang_04_banner_web_slider_800x400.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/thang_04_banner_web_slider_800x400.webp',
         },
         {
             name: 'thang_04_acer_laptop_800x400',
-            imgUrl: 'http://localhost:8889/uploads/banners/thang_04_acer_laptop_800x400.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/thang_04_acer_laptop_800x400.webp',
         },
         {
             name: 'thang_03_thu_cu_doi_moi_banner_web_slider_800x400',
-            imgUrl: 'http://localhost:8889/uploads/banners/thang_03_thu_cu_doi_moi_banner_web_slider_800x400.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/thang_03_thu_cu_doi_moi_banner_web_slider_800x400.webp',
         },
         {
             name: 'thang_03_laptop_rtx_5090_800x400',
-            imgUrl: 'http://localhost:8889/uploads/banners/thang_03_laptop_rtx_5090_800x400.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/thang_03_laptop_rtx_5090_800x400.webp',
         },
         {
             name: 'thang_02_pc_gvn_banner_web_slider_800x400',
-            imgUrl: 'http://localhost:8889/uploads/banners/thang_02_pc_gvn_banner_web_slider_800x400.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/thang_02_pc_gvn_banner_web_slider_800x400.webp',
         },
         {
             name: 'banner_web_slider_800x400_1199a3adfc23489798d4163a97f3bc62',
-            imgUrl: 'http://localhost:8889/uploads/banners/banner_web_slider_800x400_1199a3adfc23489798d4163a97f3bc62.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/banner_web_slider_800x400_1199a3adfc23489798d4163a97f3bc62.webp',
         },
         {
             name: 'thang_12_laptop_acer_swift_800x400',
-            imgUrl: 'http://localhost:8889/uploads/banners/thang_12_laptop_acer_swift_800x400.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/thang_12_laptop_acer_swift_800x400.webp',
         },
         {
             name: 'banner_homepage_acer_rtx_5000',
-            imgUrl: 'http://localhost:8889/uploads/banners/banner_homepage_acer_rtx_5000.webp',
+            imgUrl: 'http://ecommerce-technology-store.onrender.com/uploads/banners/banner_homepage_acer_rtx_5000.webp',
         },
     ];
 
